69459777c49f5d1144ed5b9218a78e8fd2780a8e
ivasm <38394268+ivasm@users.noreply.github.com>
2018-04-15 13:15:03 +0300

    Update DIV_NN_N.h
    Царь Иван выложил свою работу

--- Diskretka/DIV_NN_N.h
+++ Diskretka/DIV_NN_N.h
@@ -1,9 +1,18 @@
 #pragma once
 // N-11
 
+// Кто вызовет функцию с a<b, тот очень плохой человек
+// Смирнов Иван
 N* DIV_NN_N(N* a, N* b)
 {
-	N* result = initN();
-	result = SUB_NDN_N(SUB_NDN_N(a, b, 0), SUB_NDN_N(a, b, DIV_NN_Dk(a, b, 0)), DIV_NN_Dk(a, b, 0) + 1);
+	N* temp = DIV_NN_Dk(a, b);
+	N* result = temp;
+	N* R = SUB_NN_N(a, MUL_NN_N(temp, b)) // R = a - [a/b]*b
+	while (COM_NN_D(R, b) == 2) // R != b по определению ()
+	{
+		temp = DIV_NN_Dk(R, b);
+		result = ADD_NN_N(result, temp);
+		R = SUB_NN_N(R, MUL_NN_N(temp, b))
+	}
 	return result;
 }
